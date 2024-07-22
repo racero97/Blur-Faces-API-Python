015942e424e23eb1b59abaa8dd4b76234d5ab4da
racero97 <134523366+racero97@users.noreply.github.com>
2024-07-22 10:30:42 +0200

    Update faces.py

--- faces.py
+++ faces.py
@@ -1,4 +1,4 @@
-# Escriba (o pegue) aquí su solución
+#construir una api para preservar la privacidad en imágenes de forma que reciba la URL de una imagen y si hay personas en ella la devuelva pero con las caras borrosas (blur) con un factor de borrosidad por defecto de 3. El único endpont será /?url=url_de_la_imagen&factor=3
 import io
 
 import face_recognition
